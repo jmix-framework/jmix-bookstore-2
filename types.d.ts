3d883c315a365ba39817d391ca9f799110d2fa12
Amir Chelly <amirchelly99@gmail.com>
2024-12-10 15:15:56 +0300

    added tab for kanban

--- types.d.ts
+++ types.d.ts
@@ -23,4 +23,14 @@ declare module '*.css' {
     import { CSSResultGroup } from 'lit';
     const content: CSSResultGroup;
     export default content;
+}declare module '*.css?inline' {
+  import type { CSSResultGroup } from 'lit';
+  const content: CSSResultGroup;
+  export default content;
+}
+
+declare module 'csstype' {
+  interface Properties {
+    [index: `--${string}`]: any;
+  }
 }
