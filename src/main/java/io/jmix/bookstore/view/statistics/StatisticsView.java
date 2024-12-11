583a0508c202d31a2dafa25d95cb96a35ef9a2a2
Amir Chelly <amirchelly99@gmail.com>
2024-12-11 18:08:47 +0300

    added statistics view for pivot table

---
+++ src/main/java/io/jmix/bookstore/view/statistics/StatisticsView.java
@@ -0,0 +1,14 @@
+package io.jmix.bookstore.view.statistics;
+
+
+import com.vaadin.flow.router.Route;
+import io.jmix.bookstore.view.main.MainView;
+import io.jmix.flowui.view.*;
+
+@Route(value = "statistics", layout = MainView.class)
+@ViewController("bookstore_Statistics.view")
+@ViewDescriptor("statistics-view.xml")
+@DialogMode(width = "50em", height = "37.5em")
+public class StatisticsView extends StandardView {
+
+}
